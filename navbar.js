69b56cd836b80d8ac0c7da2b20ec78fabdf7ffd8
Axel JOLY <contact@axeljoly.fr>
2023-06-12 11:58:37 +0200

    first commit

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    var menu = document.getElementById("menu"); 
+    menu.classList.add("mMenu");   
+    
+}   
+function closeMenu(){
+    var menu = document.getElementById("menu");
+    menu.classList.remove("mMenu");  
+}
