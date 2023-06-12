595acec56b594014aa43bda41611d7d20dec702a
Axel JOLY <contact@axeljoly.fr>
2023-06-12 15:17:59 +0200

    chang 1

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ function openMenu(){
     var menu = document.getElementById("menu"); 
     menu.classList.add("mMenu");   
     
-}   
+} 
 function closeMenu(){
     var menu = document.getElementById("menu");
     menu.classList.remove("mMenu");  
